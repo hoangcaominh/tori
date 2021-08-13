648722e3abfff206b8133a560892e7b7afe9ad3a
Hoàng Cao Minh <35196884+hoangcaominh@users.noreply.github.com>
2021-08-14 00:18:24 +0700

    Update read_memory_32
    Eliminate special case

--- game/thxx.cpp
+++ game/thxx.cpp
@@ -6,20 +6,14 @@ bool read_memory_32(HANDLE process, ADDRESS_LIST address_list, void* buffer, siz
 	uint32_t address = 0;
 	BOOL success;
 
-	for (size_t i = 0; i < length - 1; ++i)
+	for (size_t i = 0; i < length; ++i)
 	{
-		success = ReadProcessMemory(process, (void*)(address + address_list[i]), &address, sizeof(address), 0);
+		success = ReadProcessMemory(process, (void*)(address + address_list[i]), (i == length - 1) ? buffer : &address, (i == length - 1) ? buffer_size : sizeof(address), 0);
 		if (!success)
 		{
 			fprintf(stderr, "Failed to get memory from address %u\n", address + address_list[i]);
 			return false;
 		}
-	}
-	success = ReadProcessMemory(process, (void*)(address + address_list[length - 1]), buffer, buffer_size, 0);
-	if (!success)
-	{
-		fprintf(stderr, "Failed to get memory from address %u\n", address + address_list[length - 1]);
-		return false;
 	}
 	return true;
 }

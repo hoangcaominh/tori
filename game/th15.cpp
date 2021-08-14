ec0678e5bade14b1714af0a8446a641fb337d44b
Hoàng Cao Minh <35196884+hoangcaominh@users.noreply.github.com>
2021-08-15 00:29:13 +0700

    [TH15] Add th15 support

--- game/th15.cpp
+++ game/th15.cpp
@@ -1,6 +1,74 @@
 #include "th15.h"
 
+TH15::TH15(HANDLE process, VERSION version) : process(process)
+{
+	switch (version)
+	{
+	case TH15::VERSION::V100B:
+		th15_addr_list = {
+			{ 0x004E7410 },
+			{ 0x004E7404 },
+			{ 0x004E9BB8, 0x16220 },
+			{ 0x004E9A68, 0x24 },
+			{ 0x004E740C },
+			{ 0x004E7414 },
+		};
+		break;
+	}
+}
+
 void TH15::reset()
 {
 
+}
+
+const char* TH15::get_difficulty()
+{
+	if (!read_memory_32(process, th15_addr_list.difficulty, (void*)&difficulty, sizeof(difficulty)))
+		fprintf(stderr, "Failed to read memory of difficulty.\n");
+	return arr_difficulty[difficulty];
+}
+
+std::string TH15::get_shottype()
+{
+	std::string shottype;
+	if (!read_memory_32(process, th15_addr_list.character, (void*)&character, sizeof(character)))
+		fprintf(stderr, "Failed to read memory of character.\n");
+	shottype += character;
+	return shottype;
+}
+
+uint16_t TH15::get_miss_count()
+{
+	if (!read_memory_32(process, th15_addr_list.p_player_state, (void*)&player_state, sizeof(player_state)))
+		fprintf(stderr, "Failed to read memory of player state.\n");
+
+	// if player_state changes form 4 to 2, increase miss_count by 1
+	if (player_state == 2 && player_state != _player_state)
+		miss_count++;
+	_player_state = player_state;
+
+	return miss_count;
+}
+
+uint16_t TH15::get_bomb_count()
+{
+	if (!read_memory_32(process, th15_addr_list.p_bomb_state, (void*)&bomb_state, sizeof(bomb_state)))
+		fprintf(stderr, "Failed to read memory of bomb state.\n");
+
+	// if bomb_state changes to 1, increase bomb_count by 1
+	if (bomb_state && bomb_state != _bomb_state)
+		bomb_count++;
+	_bomb_state = bomb_state;
+
+	return bomb_count;
+}
+
+uint64_t TH15::get_score()
+{
+	if (!read_memory_32(process, th15_addr_list.score, (void*)&score, sizeof(uint32_t)))
+		fprintf(stderr, "Failed to read memory of score.\n");
+	if (!read_memory_32(process, th15_addr_list.continue_count, (void*)&continue_count, sizeof(continue_count)))
+		fprintf(stderr, "Failed to read memory of continue.\n");
+	return score * 10 + continue_count;
 }
--- game/th15.h
+++ game/th15.h
@@ -2,10 +2,50 @@
 
 #include "thxx.h"
 
+
 class TH15 : public THXX
 {
 public:
-	void reset() override;
-private:
+	enum class VERSION
+	{
+		V100B
+	};
 
+	TH15(HANDLE, VERSION);
+
+	void reset() override;
+	const char* get_difficulty();
+	std::string get_shottype();
+	uint16_t get_miss_count();
+	uint16_t get_bomb_count();
+	uint64_t get_score();
+private:
+	HANDLE process;
+
+	// address list for th15
+	struct TH15_ADDR_LIST
+	{
+		ADDRESS_LIST difficulty;
+		ADDRESS_LIST character;
+		ADDRESS_LIST p_player_state;
+		ADDRESS_LIST p_bomb_state;
+		ADDRESS_LIST score;
+		ADDRESS_LIST continue_count;
+	} th15_addr_list;
+
+	// get_difficulty
+	const char* arr_difficulty[5] = { "Easy", "Normal", "Hard", "Lunatic", "Extra" };
+	uint16_t difficulty = 0;
+	// get shottype
+	const char* arr_character[4] = { "Reimu", "Marisa", "Sanae", "Reisen" };
+	uint8_t character = 0;
+	// get miss count
+	uint8_t player_state = 0, _player_state = 0;
+	uint16_t miss_count = 0;
+	// get bomb count
+	uint8_t bomb_state = 0, _bomb_state = 0;
+	uint16_t bomb_count = 0;
+	// get score
+	uint64_t score = 0;
+	uint8_t continue_count = 0;
 };

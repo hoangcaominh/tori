bd54a70916b5d7a6bac0ee96af241902f7279880
Hoàng Cao Minh <35196884+hoangcaominh@users.noreply.github.com>
2021-08-15 21:59:51 +0700

    [TH11] Add th11 support

--- game/th11.cpp
+++ game/th11.cpp
@@ -1,6 +1,80 @@
 #include "th11.h"
 
+TH11::TH11(HANDLE process, VERSION version) : process(process)
+{
+	switch (version)
+	{
+	case TH11::VERSION::V100A:
+		th11_addr_list = {
+			{ 0x004A5720 },
+			{ 0x004A5710 },
+			{ 0x004A5714 },
+			{ 0x004A56E4 },
+			{ 0x004A573C },
+			{ 0x004A8EB4, 0x928 },
+			{ 0x004A8D64, 0x3C }
+		};
+		break;
+	}
+}
+
 void TH11::reset()
 {
 
+}
+
+const char* TH11::get_difficulty()
+{
+	if (!read_memory_32(process, th11_addr_list.difficulty, (void*)&difficulty, sizeof(difficulty)))
+		fprintf(stderr, "Failed to read memory of difficulty.\n");
+	return arr_difficulty[difficulty];
+}
+
+const char* TH11::get_character()
+{
+	if (!read_memory_32(process, th11_addr_list.character, (void*)&character, sizeof(character)))
+		fprintf(stderr, "Failed to read memory of character.\n");
+	return arr_character[character];
+}
+
+const char* TH11::get_subshot()
+{
+	if (!read_memory_32(process, th11_addr_list.subshot, (void*)&subshot, sizeof(subshot)))
+		fprintf(stderr, "Failed to read memory of subshot.\n");
+	return arr_subshot[subshot];
+}
+
+uint64_t TH11::get_score()
+{
+	if (!read_memory_32(process, th11_addr_list.score, (void*)&score, sizeof(score)))
+		fprintf(stderr, "Failed to read memory of score.\n");
+	if (!read_memory_32(process, th11_addr_list.continue_count, (void*)&continue_count, sizeof(continue_count)))
+		fprintf(stderr, "Failed to read memory of continue.\n");
+	return (uint64_t)score * 10 + continue_count;
+}
+
+uint16_t TH11::get_miss_count()
+{
+	if (!read_memory_32(process, th11_addr_list.p_player_state, (void*)&player_state, sizeof(player_state)))
+		fprintf(stderr, "Failed to read memory of player state.\n");
+
+	// if player_state changes form 4 to 2, increase miss_count by 1
+	if (player_state == 2 && player_state != _player_state)
+		miss_count++;
+	_player_state = player_state;
+
+	return miss_count;
+}
+
+uint16_t TH11::get_bomb_count()
+{
+	if (!read_memory_32(process, th11_addr_list.p_bomb_state, (void*)&bomb_state, sizeof(bomb_state)))
+		fprintf(stderr, "Failed to read memory of bomb state.\n");
+
+	// if bomb_state changes to 1, increase bomb_count by 1
+	if (bomb_state && bomb_state != _bomb_state)
+		bomb_count++;
+	_bomb_state = bomb_state;
+
+	return bomb_count;
 }
--- game/th11.h
+++ game/th11.h
@@ -5,7 +5,49 @@
 class TH11 : public THXX
 {
 public:
-	void reset() override;
-private:
+	enum class VERSION
+	{
+		V100A
+	};
 
+	TH11(HANDLE, VERSION);
+
+	void reset() override;
+	const char* get_difficulty();
+	const char* get_character();
+	const char* get_subshot();
+	uint64_t get_score();
+	uint16_t get_miss_count();
+	uint16_t get_bomb_count();
+private:
+	HANDLE process;
+
+	// address list for th11
+	struct TH11_ADDR_LIST
+	{
+		ADDRESS_LIST difficulty;
+		ADDRESS_LIST character;
+		ADDRESS_LIST subshot;
+		ADDRESS_LIST score;
+		ADDRESS_LIST continue_count;
+		ADDRESS_LIST p_player_state;
+		ADDRESS_LIST p_bomb_state;
+	} th11_addr_list;
+
+	// get_difficulty
+	const char* arr_difficulty[5] = { "Easy", "Normal", "Hard", "Lunatic", "Extra" };
+	uint16_t difficulty = 0;
+	// get shottype
+	const char* arr_character[2] = { "Reimu", "Marisa" };
+	const char* arr_subshot[3] = { "A", "B", "C" };
+	uint8_t character = 0, subshot = 0;
+	// get miss count
+	uint8_t player_state = 0, _player_state = 0;
+	uint16_t miss_count = 0;
+	// get bomb count
+	uint8_t bomb_state = 0, _bomb_state = 0;
+	uint16_t bomb_count = 0;
+	// get score
+	uint32_t score = 0;
+	uint8_t continue_count = 0;
 };

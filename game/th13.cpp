e1b382ce5f0fdefeb7b9667c288571e7cf658a11
Hoàng Cao Minh <35196884+hoangcaominh@users.noreply.github.com>
2021-08-15 08:09:00 +0700

    [TH13] Add th13 support

--- game/th13.cpp
+++ game/th13.cpp
@@ -1,6 +1,90 @@
 #include "th13.h"
 
+TH13::TH13(HANDLE process, VERSION version) : process(process)
+{
+	switch (version)
+	{
+	case TH13::VERSION::V100C:
+		th13_addr_list = {
+			{ 0x004BE7C4 },
+			{ 0x004BE7B8 },
+			{ 0x004BE7C0 },
+			{ 0x004BE7C8 },
+			{ 0x004C22C4, 0x65C },
+			{ 0x004C2170, 0x40 },
+			{ 0x004BE831 }
+		};
+		break;
+	}
+}
+
 void TH13::reset()
 {
 
+}
+
+const char* TH13::get_difficulty()
+{
+	if (!read_memory_32(process, th13_addr_list.difficulty, (void*)&difficulty, sizeof(difficulty)))
+		fprintf(stderr, "Failed to read memory of difficulty.\n");
+	return arr_difficulty[difficulty];
+}
+
+std::string TH13::get_shottype()
+{
+	std::string shottype;
+	if (!read_memory_32(process, th13_addr_list.character, (void*)&character, sizeof(character)))
+		fprintf(stderr, "Failed to read memory of character.\n");
+	shottype += character;
+	return shottype;
+}
+
+uint16_t TH13::get_miss_count()
+{
+	if (!read_memory_32(process, th13_addr_list.p_player_state, (void*)&player_state, sizeof(player_state)))
+		fprintf(stderr, "Failed to read memory of player state.\n");
+
+	// if player_state changes form 4 to 2, increase miss_count by 1
+	if (player_state == 2 && player_state != _player_state)
+		miss_count++;
+	_player_state = player_state;
+
+	return miss_count;
+}
+
+uint16_t TH13::get_bomb_count()
+{
+	if (!read_memory_32(process, th13_addr_list.p_bomb_state, (void*)&bomb_state, sizeof(bomb_state)))
+		fprintf(stderr, "Failed to read memory of bomb state.\n");
+
+	// if bomb_state changes to 1, increase bomb_count by 1
+	if (bomb_state && bomb_state != _bomb_state)
+		bomb_count++;
+	_bomb_state = bomb_state;
+
+	return bomb_count;
+}
+
+uint16_t TH13::get_trance_count()
+{
+	if (!read_memory_32(process, th13_addr_list.p_player_state, (void*)&player_state, sizeof(player_state)))
+		fprintf(stderr, "Failed to read memory of player state.\n");
+	if (!read_memory_32(process, th13_addr_list.trance_state, (void*)&trance_state, sizeof(trance_state)))
+		fprintf(stderr, "Failed to read memory of trance state.\n");
+
+	// If trance is over, check if the player dies
+	if ((!trance_state && trance_state != _trance_state) && player_state != 2)
+		trance_count++;
+	_trance_state = trance_state;
+
+	return trance_count;
+}
+
+uint64_t TH13::get_score()
+{
+	if (!read_memory_32(process, th13_addr_list.score, (void*)&score, sizeof(uint32_t)))
+		fprintf(stderr, "Failed to read memory of score.\n");
+	if (!read_memory_32(process, th13_addr_list.continue_count, (void*)&continue_count, sizeof(continue_count)))
+		fprintf(stderr, "Failed to read memory of continue.\n");
+	return score * 10 + continue_count;
 }
--- game/th13.h
+++ game/th13.h
@@ -5,7 +5,51 @@
 class TH13 : public THXX
 {
 public:
-	void reset() override;
-private:
+	enum class VERSION
+	{
+		V100C
+	};
 
+	TH13(HANDLE, VERSION);
+
+	void reset() override;
+	const char* get_difficulty();
+	std::string get_shottype();
+	uint16_t get_miss_count();
+	uint16_t get_bomb_count();
+	uint16_t get_trance_count();
+	uint64_t get_score();
+private:
+	HANDLE process;
+
+	// address list for th13
+	struct TH13_ADDR_LIST
+	{
+		ADDRESS_LIST difficulty;
+		ADDRESS_LIST character;
+		ADDRESS_LIST score;
+		ADDRESS_LIST continue_count;
+		ADDRESS_LIST p_player_state;
+		ADDRESS_LIST p_bomb_state;
+		ADDRESS_LIST trance_state;
+	} th13_addr_list;
+
+	// get_difficulty
+	const char* arr_difficulty[5] = { "Easy", "Normal", "Hard", "Lunatic", "Extra" };
+	uint16_t difficulty = 0;
+	// get shottype
+	const char* arr_character[4] = { "Reimu", "Marisa", "Sanae", "Youmu" };
+	uint8_t character = 0;
+	// get miss count
+	uint8_t player_state = 0, _player_state = 0;
+	uint16_t miss_count = 0;
+	// get bomb count
+	uint8_t bomb_state = 0, _bomb_state = 0;
+	uint16_t bomb_count = 0;
+	// get trance count
+	uint8_t trance_state = 0, _trance_state = 0;
+	uint16_t trance_count = 0;
+	// get score
+	uint64_t score = 0;
+	uint8_t continue_count = 0;
 };

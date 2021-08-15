1b8c65e333ad75c80144725ecf88d4ced8a1951d
Hoàng Cao Minh <35196884+hoangcaominh@users.noreply.github.com>
2021-08-15 18:20:01 +0700

    [TH16] Add th16 support

--- game/th16.cpp
+++ game/th16.cpp
@@ -1,6 +1,94 @@
 #include "th16.h"
 
+TH16::TH16(HANDLE process, VERSION version) : process(process)
+{
+	switch (version)
+	{
+	case TH16::VERSION::V100A:
+		th16_addr_list = {
+			{ 0x004A57B4 },
+			{ 0x004A57A4 },
+			{ 0x004A57AC },
+			{ 0x004A57B0 },
+			{ 0x004A57B8 },
+			{ 0x004A6EF8, 0x165A8 },
+			{ 0x004A6DA8, 0x30 },
+			{ 0x004A6DA4, 0x30 }
+		};
+		break;
+	}
+}
+
 void TH16::reset()
 {
 
+}
+
+const char* TH16::get_difficulty()
+{
+	if (!read_memory_32(process, th16_addr_list.difficulty, (void*)&difficulty, sizeof(difficulty)))
+		fprintf(stderr, "Failed to read memory of difficulty.\n");
+	return arr_difficulty[difficulty];
+}
+
+const char* TH16::get_character()
+{
+	if (!read_memory_32(process, th16_addr_list.character, (void*)&character, sizeof(character)))
+		fprintf(stderr, "Failed to read memory of character.\n");
+	return arr_character[character];
+}
+
+const char* TH16::get_season()
+{
+	if (!read_memory_32(process, th16_addr_list.season, (void*)&season, sizeof(season)))
+		fprintf(stderr, "Failed to read memory of weapon.\n");
+	return arr_season[season];
+}
+
+uint16_t TH16::get_miss_count()
+{
+	if (!read_memory_32(process, th16_addr_list.p_player_state, (void*)&player_state, sizeof(player_state)))
+		fprintf(stderr, "Failed to read memory of player state.\n");
+
+	// if player_state changes form 4 to 2, increase miss_count by 1
+	if (player_state == 2 && player_state != _player_state)
+		miss_count++;
+	_player_state = player_state;
+
+	return miss_count;
+}
+
+uint16_t TH16::get_bomb_count()
+{
+	if (!read_memory_32(process, th16_addr_list.p_bomb_state, (void*)&bomb_state, sizeof(bomb_state)))
+		fprintf(stderr, "Failed to read memory of bomb state.\n");
+
+	// if bomb_state changes to 1, increase bomb_count by 1
+	if (bomb_state && bomb_state != _bomb_state)
+		bomb_count++;
+	_bomb_state = bomb_state;
+
+	return bomb_count;
+}
+
+uint16_t TH16::get_release_count()
+{
+	if (!read_memory_32(process, th16_addr_list.p_release_state, (void*)&release_state, sizeof(release_state)))
+		fprintf(stderr, "Failed to read memory of bomb state.\n");
+
+	// similar to bomb state
+	if (release_state && release_state != _release_state)
+		release_count++;
+	_release_state = release_state;
+
+	return release_count;
+}
+
+uint64_t TH16::get_score()
+{
+	if (!read_memory_32(process, th16_addr_list.score, (void*)&score, sizeof(uint32_t)))
+		fprintf(stderr, "Failed to read memory of score.\n");
+	if (!read_memory_32(process, th16_addr_list.continue_count, (void*)&continue_count, sizeof(continue_count)))
+		fprintf(stderr, "Failed to read memory of continue.\n");
+	return score * 10 + continue_count;
 }
--- game/th16.h
+++ game/th16.h
@@ -5,7 +5,54 @@
 class TH16 : public THXX
 {
 public:
-	void reset() override;
-private:
+	enum class VERSION
+	{
+		V100A
+	};
 
+	TH16(HANDLE, VERSION);
+
+	void reset() override;
+	const char* get_difficulty();
+	const char* get_character();
+	const char* get_season();
+	uint16_t get_miss_count();
+	uint16_t get_bomb_count();
+	uint16_t get_release_count();
+	uint64_t get_score();
+private:
+	HANDLE process;
+
+	// address list for th16
+	struct TH16_ADDR_LIST
+	{
+		ADDRESS_LIST difficulty;
+		ADDRESS_LIST character;
+		ADDRESS_LIST season;
+		ADDRESS_LIST score;
+		ADDRESS_LIST continue_count;
+		ADDRESS_LIST p_player_state;
+		ADDRESS_LIST p_bomb_state;
+		ADDRESS_LIST p_release_state;
+	} th16_addr_list;
+
+	// get_difficulty
+	const char* arr_difficulty[5] = { "Easy", "Normal", "Hard", "Lunatic", "Extra" };
+	uint16_t difficulty = 0;
+	// get shottype
+	const char* arr_character[4] = { "Reimu", "Cirno", "Aya", "Marisa" };
+	const char* arr_season[4] = { "Spring", "Summer", "Autumn", "Winter" };
+	uint8_t character = 0, season = 0;
+	// get miss count
+	uint8_t player_state = 0, _player_state = 0;
+	uint16_t miss_count = 0;
+	// get bomb count
+	uint8_t bomb_state = 0, _bomb_state = 0;
+	uint16_t bomb_count = 0;
+	// get reelase count
+	uint8_t release_state = 0, _release_state = 0;
+	uint16_t release_count = 0;
+	// get score
+	uint64_t score = 0;
+	uint8_t continue_count = 0;
 };

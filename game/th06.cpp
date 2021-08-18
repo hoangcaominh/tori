4467661e48d208d08dcf1fa52b3d93a9abbf5fd0
Hoàng Cao Minh <35196884+hoangcaominh@users.noreply.github.com>
2021-08-18 20:18:42 +0700

    [TH06] Add th06 support

--- game/th06.cpp
+++ game/th06.cpp
@@ -1,6 +1,71 @@
 #include "th06.h"
 
+TH06::TH06(HANDLE process, VERSION version) : process(process)
+{
+	switch (version)
+	{
+	case TH06::VERSION::V102H:
+		th06_addr_list = {
+			{ 0x0069BCB0 },
+			{ 0x0069D4BD },
+			{ 0x0069D4BE },
+			{ 0x0069BCA0 },
+			{ 0x006CB000 },
+			{ 0x0069BCC4 }
+		};
+		break;
+	}
+}
+
 void TH06::reset()
 {
 
+}
+
+const char* TH06::get_difficulty()
+{
+	if (!read_memory_32(process, th06_addr_list.difficulty, (void*)&difficulty, sizeof(difficulty)))
+		fprintf(stderr, "Failed to read memory of difficulty.\n");
+	return arr_difficulty[difficulty];
+}
+
+const char* TH06::get_character()
+{
+	if (!read_memory_32(process, th06_addr_list.character, (void*)&character, sizeof(character)))
+		fprintf(stderr, "Failed to read memory of character.\n");
+	return arr_character[character];
+}
+
+const char* TH06::get_subshot()
+{
+	if (!read_memory_32(process, th06_addr_list.subshot, (void*)&subshot, sizeof(subshot)))
+		fprintf(stderr, "Failed to read memory of subshot.\n");
+	return arr_subshot[subshot];
+}
+
+uint32_t TH06::get_score()
+{
+	if (!read_memory_32(process, th06_addr_list.score, (void*)&score, sizeof(score)))
+		fprintf(stderr, "Failed to read memory of score.\n");
+	return score;
+}
+
+uint16_t TH06::get_miss_count()
+{
+	if (!read_memory_32(process, th06_addr_list.deathbomb_window, (void*)&deathbomb_window, sizeof(deathbomb_window)))
+		fprintf(stderr, "Failed to read memory of deathbomb window.\n");
+
+	// for some reasons when the player dies the deathbomb window gets set to 0
+	if (deathbomb_window == 0 && deathbomb_window != _deathbomb_window)
+		miss_count++;
+	_deathbomb_window = deathbomb_window;
+	
+	return miss_count;
+}
+
+uint16_t TH06::get_bomb_count()
+{
+	if (!read_memory_32(process, th06_addr_list.bomb_count, (void*)&bomb_count, sizeof(bomb_count)))
+		fprintf(stderr, "Failed to read memory of bomb count.\n");
+	return bomb_count;
 }
--- game/th06.h
+++ game/th06.h
@@ -5,7 +5,46 @@
 class TH06 : public THXX
 {
 public:
-	void reset() override;
-private:
+	enum class VERSION
+	{
+		V102H
+	};
 
+	TH06(HANDLE, VERSION);
+
+	void reset() override;
+	const char* get_difficulty();
+	const char* get_character();
+	const char* get_subshot();
+	uint32_t get_score();
+	uint16_t get_miss_count();
+	uint16_t get_bomb_count();
+private:
+	HANDLE process;
+
+	// address list for th06
+	struct TH06_ADDR_LIST
+	{
+		ADDRESS_LIST difficulty;
+		ADDRESS_LIST character;
+		ADDRESS_LIST subshot;
+		ADDRESS_LIST score;
+		ADDRESS_LIST deathbomb_window;
+		ADDRESS_LIST bomb_count;
+	} th06_addr_list;
+
+	// get_difficulty
+	const char* arr_difficulty[5] = { "Easy", "Normal", "Hard", "Lunatic", "Extra" };
+	uint16_t difficulty = 0;
+	// get shottype
+	const char* arr_character[2] = { "Reimu", "Marisa" };
+	const char* arr_subshot[2] = { "A", "B" };
+	uint8_t character = 0, subshot = 0;
+	// get miss count
+	uint8_t deathbomb_window = 0, _deathbomb_window = 0;
+	uint16_t miss_count = 0;
+	// get bomb count
+	uint16_t bomb_count = 0;
+	// get score
+	uint32_t score = 0;
 };

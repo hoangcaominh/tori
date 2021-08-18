6e87508b8cbb3f7cc84ad477550f7bc5c32a8136
Hoàng Cao Minh <35196884+hoangcaominh@users.noreply.github.com>
2021-08-18 19:02:11 +0700

    [TH08] Add th08 support

--- game/th08.cpp
+++ game/th08.cpp
@@ -1,6 +1,79 @@
 #include "th08.h"
 
+TH08::TH08(HANDLE process, VERSION version) : process(process)
+{
+	switch (version)
+	{
+	case TH08::VERSION::V100D:
+		th08_addr_list = {
+			{ 0x0160F538 },
+			{ 0x0164D0B1 },
+			{ 0x004E4850 },
+			{ 0x0160F510, 0x0 },
+			{ 0x004D77DC },
+			{ 0x0164CFA4 },
+			{ 0x0164CFA8 },
+			{ 0x0164CFAC },
+			{ 0x0160F510, 0x1C }
+		};
+		break;
+	}
+}
+
 void TH08::reset()
 {
 
+}
+
+const char* TH08::get_difficulty()
+{
+	if (!read_memory_32(process, th08_addr_list.difficulty, (void*)&difficulty, sizeof(difficulty)))
+		fprintf(stderr, "Failed to read memory of difficulty.\n");
+	return arr_difficulty[difficulty];
+}
+
+const char* TH08::get_character()
+{
+	if (!read_memory_32(process, th08_addr_list.character, (void*)&character, sizeof(character)))
+		fprintf(stderr, "Failed to read memory of character.\n");
+	return arr_character[character];
+}
+
+const char* TH08::get_stage()
+{
+	if (!read_memory_32(process, th08_addr_list.stage, (void*)&stage, sizeof(stage)))
+		fprintf(stderr, "Failed to read memory of stage.\n");
+	return arr_stage[stage];
+}
+
+uint64_t TH08::get_score()
+{
+	if (!read_memory_32(process, th08_addr_list.score, (void*)&score, sizeof(score)))
+		fprintf(stderr, "Failed to read memory of score.\n");
+	if (!read_memory_32(process, th08_addr_list.continue_count, (void*)&continue_count, sizeof(continue_count)))
+		fprintf(stderr, "Failed to read memory of continue.\n");
+	return (uint64_t)score * 10 + ((continue_count > 9) ? 9 : continue_count);
+}
+
+uint16_t TH08::get_miss_count()
+{
+	if (!read_memory_32(process, th08_addr_list.miss_count, (void*)&miss_count, sizeof(miss_count)))
+		fprintf(stderr, "Failed to read memory of miss count.\n");
+	return miss_count;
+}
+
+uint16_t TH08::get_bomb_count()
+{
+	if (!read_memory_32(process, th08_addr_list.bomb_count, (void*)&bomb_count, sizeof(bomb_count)))
+		fprintf(stderr, "Failed to read memory of bomb count.\n");
+	if (!read_memory_32(process, th08_addr_list.deathbomb_count, (void*)&deathbomb_count, sizeof(deathbomb_count)))
+		fprintf(stderr, "Failed to read memory of deathbomb count.\n");
+	return bomb_count + deathbomb_count;
+}
+
+uint16_t TH08::get_spell_captured_count()
+{
+	if (!read_memory_32(process, th08_addr_list.spell_captured_count, (void*)&spell_captured_count, sizeof(spell_captured_count)))
+		fprintf(stderr, "Failed to read memory of spell captured count.\n");
+	return spell_captured_count;
 }
--- game/th08.h
+++ game/th08.h
@@ -5,7 +5,54 @@
 class TH08 : public THXX
 {
 public:
-	void reset() override;
-private:
+	enum class VERSION
+	{
+		V100D
+	};
 
+	TH08(HANDLE, VERSION);
+
+	void reset() override;
+	const char* get_difficulty();
+	const char* get_character();
+	const char* get_stage();
+	uint64_t get_score();
+	uint16_t get_miss_count();
+	uint16_t get_bomb_count();
+	uint16_t get_spell_captured_count();
+private:
+	HANDLE process;
+
+	// address list for th08
+	struct TH08_ADDR_LIST
+	{
+		ADDRESS_LIST difficulty;
+		ADDRESS_LIST character;
+		ADDRESS_LIST stage;
+		ADDRESS_LIST score;
+		ADDRESS_LIST continue_count;
+		ADDRESS_LIST miss_count;
+		ADDRESS_LIST bomb_count;
+		ADDRESS_LIST deathbomb_count;
+		ADDRESS_LIST spell_captured_count;
+	} th08_addr_list;
+
+	// get_difficulty
+	const char* arr_difficulty[5] = { "Easy", "Normal", "Hard", "Lunatic", "Extra" };
+	uint8_t difficulty = 0;
+	// get shottype
+	const char* arr_character[12] = { "Reimu & Yukari", "Marisa & Alice", "Sakuya & Remilia", "Youmu & Yuyuko", "Reimu", "Yukari", "Marisa", "Alice", "Sakuya", "Remilia", "Youmu", "Yuyuko" };
+	uint8_t character = 0;
+	// get stage
+	const char* arr_stage[9] = { "1", "2", "3", "4A", "4B", "5", "6A", "6B", "Ex" };
+	uint8_t stage;
+	// get score
+	uint32_t score = 0;
+	uint8_t continue_count = 0;
+	// get miss count
+	uint16_t miss_count = 0;
+	// get bomb count
+	uint16_t bomb_count = 0, deathbomb_count = 0;
+	// get spell captured count
+	uint16_t spell_captured_count = 0;
 };
